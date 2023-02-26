1f24ca556d2685e54c33efab0453473843866416
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2023-02-26 16:29:38 +0600

    product content page

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens:{
+        sm:'576px',
+        md:'768px',
+        lg:'992px',
+        xl:'1200px'
+      },
+
+      container:{
+        center:true,
+        padding:'1rem'
+
+      },
+      fontFamily:{
+        poppins: "'Poppins', sans-serif",
+        roboto:"'Roboto', sans-serif"
+      },
+      colors:{
+      'primary':"#FD3D57"
+      }
+    },
+  },
+  variants:{
+    extend:{
+      display:['group-hover'],
+      visibility:['group-hover'],
+    },
+  },
+  plugins: [],
+}
